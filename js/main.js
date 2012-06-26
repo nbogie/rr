2cccd2fb2c02d626899aa6ad7725cbcb279ea204
Neill Bogie <neillbogie@googlemail.com>
2012-06-26 03:25:22 +0100

    first pass of random li selection (straight jquery)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   var generateMove = function(){
     $('#movedisplay').text("new move was generated");
+    var list = $('#movelist li').toArray();
+    var randomnum = Math.floor(Math.random()*list.length) + 1;
+    var randomitem = list[randomnum];
+    var x = $("#movelist li:nth-child(" + randomnum + ")");
+    $('#movedisplay').text(x.first().text());
   }; 
+
+
   $('#suggestbtn').click(function() { generateMove(); } );
+  $('#movelisthdr').click(function() { $('#movelist').toggle(); } );
+
+  generateMove();
 });
