3e086771583e7a737e25f079e2a3270f07612108
Neill Bogie <neillbogie@googlemail.com>
2012-06-29 18:20:59 +0100

    added list editing capability (parses lines of text area)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   var generateMove = function(){
     $('#movedisplay').text("new move was generated");
     var list = $('#movelist li').toArray();
@@ -8,12 +9,55 @@ $(document).ready(function() {
     $('#movedisplay').text(x.first().text());
   }; 
 
+  var showAll = function() { 
+    $(".landscape").show();
+    $(".portrait").show();
+  };
 
-  $('#suggestbtn').click(function() { generateMove(); } );
-  $('#movelisthdr').click(function() { $('#movelist').toggle(); } );
+  var rotate = function() { 
+    $(".landscape").toggle();
+    $(".portrait").toggle();
+  };
+
+  var checkIfBlank = function(value, index, ar) {
+    return ( (typeof value === 'string') && 
+             !(value.replace(/^\s+|\s+$/g, '') == '')
+           );
+  }
+
+  var parseTextBox = function() { 
+    var t = $('#movelisttextarea').val();
+    lines = t.split(/\r\n|\r|\n/);
+    lines = lines.filter(checkIfBlank);
+    return lines;
+  };
+
+  var populateMovesFromTextBox = function() {
+    var lines = parseTextBox();
+
+    var cList = $('#movelist')
+    $("#movelist li").remove();
+    $.each(lines, function(i)
+    {
+        var li = $('<li/>')
+            .appendTo(cList);
+        var aaa = $('<a/>')
+            .text(lines[i])
+            .appendTo(li);
+    });
+  };
+
   window.setInterval(function() {
     generateMove();
   }, 1000);
 
+  $('#suggestbtn').click(function()  { generateMove(); } );
+  $('#rotateBtn').click(function()  { rotate(); } );
+  $('#movelisthdr').click(function() { $('#movelist').toggle(); } );
+  $('#parseMovesBtn').click(function() { populateMovesFromTextBox(); } );
+
+  //showAll();
+  rotate();
+  populateMovesFromTextBox();
   generateMove();
 });
